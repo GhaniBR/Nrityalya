b6a6bfe0c9a0efbcc63d43ce067ff88b2633206b
Ghaniya <ghaniyabegum@gmail.com>
2025-04-19 12:40:33 +0530

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    ssr: true,
+    ssrEmitAssets: true,
+    rollupOptions: {
+      output: {
+        format: "esm",
+      },
+    },
+  },
 });
